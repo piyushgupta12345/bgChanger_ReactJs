72534157a62ee15ce0832eeba2e3255d898b8cab
Piyush Gupta <124340622+piyushgupta12345@users.noreply.github.com>
2023-10-05 23:30:02 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,14 @@ function App() {
     <div className="w-full h-screen flex justify-center items-end" style={{backgroundColor:color}}>
       <div className="w-1/2 h-16 rounded-full flex mb-28 justify-center items-center gap-2 bg-white">
       <button className="bg-red-500 px-4 py-1 rounded-full" onClick={()=> setColor("red")}>Red</button>
-      <button onClick={()=> setColor("green")}>Green</button>
-      <button onClick={()=> setColor("blue")}>Blue</button>
-      <button onClick={()=> setColor("olive")}>Olive</button>
-      <button onClick={()=> setColor("Gray")}>Gray</button>
-      <button onClick={()=> setColor("yellow")}>Yellow</button>
-      <button onClick={()=> setColor("pink")}>Pink</button>
-      <button onClick={()=> setColor("purple")}>Purple</button>
-      <button onClick={()=> setColor("lavender")}>Lavender</button>
-      <button onClick={()=> setColor("orange")}>Orange</button>
-      <button onClick={()=> setColor("black")}>Black</button>
+      <button className="bg-green-500 px-4 py-1 rounded-full" onClick={()=> setColor("green")}>Green</button>
+      <button className="bg-blue-500 px-4 py-1 rounded-full" onClick={()=> setColor("blue")}>Blue</button>
+      <button className="bg-gray-500 px-4 py-1 rounded-full" onClick={()=> setColor("gray")}>Gray</button>
+      <button className="bg-yellow-500 px-4 py-1 rounded-full" onClick={()=> setColor("yellow")}>Yellow</button>
+      <button className="bg-pink-500 px-4 py-1 rounded-full" onClick={()=> setColor("pink")}>Pink</button>
+      <button className="bg-purple-500 px-4 py-1 rounded-full" onClick={()=> setColor("purple")}>Purple</button>
+      <button className="bg-lavender-500 px-4 py-1 rounded-full" onClick={()=> setColor("lavender")}>Lavender</button>
+      <button className="bg-orange-500 px-4 py-1 rounded-full" onClick={()=> setColor("orange")}>Orange</button>
       </div>
     </div>
   );
